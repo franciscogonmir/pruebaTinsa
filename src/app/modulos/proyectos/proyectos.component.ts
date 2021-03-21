9f6a5f3c756590a1b5882dc47cd9d53978fad7ac
franciscogonmir <franciscogonmirdeveloper@gmail.com>
2021-03-21 02:59:42 +0100

    clean code

--- src/app/modulos/proyectos/proyectos.component.ts
+++ src/app/modulos/proyectos/proyectos.component.ts
@@ -1,25 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RecursosService } from '../../services/recursos.service';
 import { Proyecto } from './proyectos-mapper';
+import { Subscription } from 'rxjs';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
-export class ProyectosComponent implements OnInit {
+export class ProyectosComponent implements OnInit ,OnDestroy{
 
   proyectos:Proyecto[];
-  displayedColumns = ["id","startYear","endYear","modified","title"];
+  subsProyectos:Subscription;
+  ColumnasAMostrar = ["id","startYear","endYear","modified","title"];
 
-  constructor(private route:ActivatedRoute,private characterService:RecursosService) { }
+  constructor(private route:ActivatedRoute,private characterService:RecursosService,private location:Location) { }
+
+  ngOnDestroy(): void {
+    this.subsProyectos.unsubscribe();
+  }
 
   ngOnInit(): void {
-    this.characterService.getProyectos(this.route.snapshot.paramMap.get("id")).subscribe(response => {
+    this.subsProyectos = this.characterService.getProyectos(this.route.snapshot.paramMap.get("id")).subscribe(response => {
       this.proyectos = response.data.results;
       console.warn(this.proyectos)
     })
+  }
+
+  irAtras(){
+    this.location.back();
   }
 
 }
